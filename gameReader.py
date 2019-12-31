2b414817271b0587b0c378200d5bda88197237fe
Nathan Rueschenberg <nathanfms@gmail.com>
2019-12-30 20:49:26 -0800

    further parsing progress, new count star feature w/ opencv

--- gameReader.py
+++ gameReader.py
@@ -10,6 +10,45 @@ import os
 from google.cloud import vision
 from google.cloud.vision import types
 
+#Stars 1 - 15
+starValues = [2, 4, 6, 8, 10, 13, 16, 19, 22, 25, 28, 31, 34, 37, 40]
+
+starsOver15 = [
+    [47, 49, 51, 53, 55], #16
+    [54, 58, 62, 66, 70], #17
+    [61, 67, 73, 79, 85], #18
+    [68, 76, 84, 92, 100], #19
+    [75, 85, 95, 105, 115], #20
+    [94, 106, 118, 130], #21
+    [103, 117, 131, 145] #22
+]
+
+def countStars():
+    img = cv2.imread('./temp-vision-files/star.png')
+
+    output = img.copy()
+    output = cv2.cvtColor(output, cv2.COLOR_BGR2HSV)
+    output[:, :, 0] = 0
+    output[:, :, 2] = 0
+
+    star = cv2.imread('./assets/star.png')
+    starCopy = star.copy()
+    starCopy = cv2.cvtColor(starCopy, cv2.COLOR_BGR2HSV)
+    starCopy[:, :, 0] = 0
+    starCopy[:, :, 2] = 0
+
+    res = cv2.matchTemplate(output, starCopy, cv2.TM_CCOEFF_NORMED)
+    threshold = 0.85
+    loc = np.where(res >= threshold)
+    found = 0
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(output, pt, (pt[0] + 10, pt[1] + 10), (0,0,255), 2)
+        found += 1
+    return found
+    # cv2.imshow('asdf', output)
+    # print(found)
+    # cv2.waitKey(0)
+
 def findTop(img):
     template = cv2.imread('assets/top.png', 0)
     w, h = template.shape[::-1]
@@ -70,6 +109,8 @@ def updateEquipValues(text, eqp, statName, isPotential):
         substring = text
         if text.count('+') == 3:
             substring = text[:text.rfind('+')]
+            flame = text[text.find('('):]
+            eqp.flame.update({statName : flame[flame.find('+') + 1:flame.rfind('+')]})
         eqp.base.update({statName : text[text.find('(') + 1:substring.rfind('+')]})
     else:
         val = text[text.find('+') + 1:]
@@ -108,9 +149,9 @@ def parseEquip(ocrString):
             updateEquipValues(word, eqp, 'ATK', isPotential)
         elif 'Magic Attack' in word:
             updateEquipValues(word, eqp, 'MATK', isPotential)
-        elif 'HP' in word:
+        elif 'HP' in word and 'recover' not in word:
             updateEquipValues(word, eqp, 'HP', isPotential)
-        elif 'MP' in word:
+        elif 'MP' in word and 'recover' not in word:
             updateEquipValues(word, eqp, 'MP', isPotential)
         elif 'Defense' in word:
             updateEquipValues(word, eqp, 'DEF', isPotential)
@@ -128,9 +169,10 @@ def parseEquip(ocrString):
             updateEquipValues(word, eqp, 'DMG', isPotential)
         elif 'Ignore' in word:
             updateEquipValues(word, eqp, 'IGNORE', isPotential)
-        elif 'AI' in word or 'Al' in word or 'All' in word or 'AlI' in word or 'AIl' in word:
+        elif ('AI' in word or 'Al' in word or 'All' in word or 'AlI' in word or 'AIl' in word) and 'gain' not in word:
             updateEquipValues(word, eqp, 'ALL', isPotential)
     print(eqp.base)
+    print(eqp.flame)
     print(eqp.mpot)
     # for word in partition[idx:]:
     #     if 'Type' in word:
@@ -163,7 +205,7 @@ def screen_record():
     client = vision.ImageAnnotatorClient()
     i = 0
     lastCursorPos = win32gui.GetCursorInfo()[2]
-    while(i < 2):
+    while(i < 1):
         cursorPos = win32gui.GetCursorInfo()[2]
         # printscreen = np.array(ImageGrab.grab(bbox=(0,40,800,640)))
         winder = win32gui.FindWindow(None, "Maplestory")
@@ -188,6 +230,14 @@ def screen_record():
             equipImg = ImageGrab.grab(bbox=equipCoords)
             # print(equipImg)
             if(equipImg.width > 0 and equipImg.height > 0):
+
+                starCoords = (equipCoords[0], equipCoords[1], equipCoords[2], equipCoords[1] + 70)
+                print(starCoords)
+                starImg = ImageGrab.grab(bbox=starCoords)
+                star = np.array(starImg)
+                starColor = cv2.cvtColor(star, cv2.COLOR_BGR2RGB)
+                cv2.imwrite('./temp-vision-files/star.png', starColor)
+
                 equipWindow = np.array(equipImg)
                 color = cv2.cvtColor(equipWindow, cv2.COLOR_BGR2RGB)
                 # cv2.imshow('window',color)
@@ -244,3 +294,4 @@ def screen_record():
 
 screen_record()
 
+yellow = [(0, 180, 250), (0, 210, 255)]
